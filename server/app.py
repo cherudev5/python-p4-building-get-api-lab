ddc204c64570407be79b70558cd2ca1c3050fbf9
Cheruiyot Kigen David <cheruiyotscripts@gmail.com>
2024-04-11 16:01:34 +0300

    completed assignmnet

--- server/app.py
+++ server/app.py
@@ -20,19 +20,59 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries=[]
+    for bakery in Bakery.query.all():
+        bakery_dict={
+            "id":bakery.id,
+            "name":bakery.name,
+            "created_at":bakery.created_at
+            
+        }
+        bakeries.append(bakery_dict)
+    
+    return jsonify(bakeries),200
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    bakery_data = {
+        'id': bakery.id,
+        'name': bakery.name,
+        'created_at':bakery.created_at,
+        'baked_goods':[good.name for good in bakery.baked_goods]
+    }
+    return jsonify(bakery_data)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    goods_list = []
+    for good in baked_goods:
+        good_data = {
+            'id': good.id,
+            'name': good.name,
+            'price': good.price,
+            'created_at': good.created_at,
+            'bakery_id': good.bakery_id
+        }
+        goods_list.append(good_data)
+    return jsonify(goods_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive_good:
+        good_data = {
+            'id': most_expensive_good.id,
+            'name': most_expensive_good.name,
+            'price': most_expensive_good.price,
+            'created_at': most_expensive_good.created_at,
+            'bakery_id': most_expensive_good.bakery_id
+        }
+        return jsonify(good_data)
+    else:
+        return jsonify({"error": "No baked goods found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
